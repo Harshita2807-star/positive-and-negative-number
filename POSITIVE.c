a4f39971a19a7651bf196f7b147caa10cddbb421
harshita purohit <harshitapurohit2807@gmail.com>
2026-08-28 09:04:32 -0700

    Add files via upload

---
+++ POSITIVE.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+ int main()
+ {
+     int i;
+     printf("enter value for i");
+     scanf("%d",&i);
+        if(i>=0)
+        {
+             printf("number is positive");
+        }
+          else
+          {
+              printf("number is negative");
+          }
+           return 0;
+ }
